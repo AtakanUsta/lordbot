d88ab5085b153acd030187deadcdfee390e60b9c
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-05 22:56:53 +0300

    Create yardım-k.js

---
+++ komutlar/yardım-k.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+//kod başlangıç
+
+
+
+
+
+
+       
+        let yardımkembed = new Discord.RichEmbed()
+            .setAuthor("~ K U L L A N I C I ~")
+            .setColor("#00ff08")
+            .addField("Bilgilerine Bakmak İçin :", `.bilgilerim`)
+            .addField("Bot Kurucunun Bilgilerine Bakmak İçin :", `.botkurucu`)
+            
+
+        message.channel.send(yarımkembed);
+    
+
+
+
+
+
+
+//kod bitiş
+
+}
+
+module.exports.help = {
+    name: "yardım-k"
+}

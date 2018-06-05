b15a44694bf91f7fdcd907bd6f5f8dc8d6ad97c5
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-05 22:08:10 +0300

    Update yaz.js

--- komutlar/yaz.js
+++ komutlar/yaz.js
@@ -10,6 +10,10 @@ module.exports.run = async (bot, message, args) => {
 
     const sayMessage = args.join(" ");
  
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":warning: `Bunu Yapmaya Yetkin Yok!`");
+    
+    if(!sayMessage) return message.channel.send(":warning: `Lütfen Yazmamı İstediğinide Yaz!`");
+    
     message.delete().catch(O_o=>{}); 
  
     message.channel.send(sayMessage);

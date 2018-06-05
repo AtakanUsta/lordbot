ccbd0c4d6b521b83d5581d31bcdc158d6711fa60
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-05 23:02:12 +0300

    Create yardım-a.js

---
+++ komutlar/yardım-a.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+//kod başlangıç
+
+
+
+
+
+
+       
+        let yardimaembed = new Discord.RichEmbed()
+            .setAuthor("~ A D M İ N ~")
+            .setColor("#ff0000")
+            .addField(".yaz [mesaj]", `Bota Mesaj Yazdırmaya Yarar!`)
+            .addField(".temizle [sayı]", `Bota Mesaj Yazdırmaya Yarar!`)
+            .addField(".sustur [kullanıcı] [süre (milisaniye biçiminden)]", `Kullanıcıyı Belli Süre Susturmaya Yarar`)
+            .addField(".uyar [kullanıcı]", `BETA AŞAMASINDA`)
+
+
+        message.channel.send(yardimaembed);
+    
+
+
+
+
+
+
+//kod bitiş
+
+}
+
+module.exports.help = {
+    name: "yardım-a"
+}

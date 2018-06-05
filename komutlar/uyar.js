ee8c91d96b33cf8b6c3981d4310f3e9bee6458a8
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-05 23:33:38 +0300

    Create uyar.js

---
+++ komutlar/uyar.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+            //başlangıç
+            
+            
+            
+            
+            
+            let reason = args.slice(1).join(" ");
+            let user = message.mentions.users.first();
+            let modlog = bot.channels.find(`name`, `lordlog`);
+            
+            if(!modlog) return message.reply(":warning: **lordlog** `Adında Bir Kanal Bulamıyorum Lütfen Kontrol Et!`");
+            if(message.mentions.users.size < 1) return message.reply(":warning: `Lütfen Uyarmak İstediğin Kullanıcının İsminide Gir`").catch(console.error);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //bitiş
+
+}
+
+module.exports.help = {
+    name: "bilgilerim"
+}

c55be2c53d7798deb559174b47df635ec721151b
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-05 17:51:15 +0300

    Update clear.js

--- komutlar/clear.js
+++ komutlar/clear.js
@@ -3,13 +3,13 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!clear 15
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("oof.");
-  if(!args[0]) return message.channel.send("oof");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":warning: `Bunu Yapmaya Yetkin Yok!`");
+  if(!args[0]) return message.channel.send(":warning: `Silinecek Bir Mesaj Yok!`");
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+    message.channel.send(`:white_check_mark: ${args[0]} `Adet Mesaj Silindi` `).then(msg => msg.delete(5000));
   });
 }
 
 module.exports.help = {
-  name: "clear"
+  name: "temizle"
 }

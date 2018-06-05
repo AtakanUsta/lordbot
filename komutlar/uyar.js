55e75d1787dcaad69aa2bc710eb71bb14da794a6
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-05 23:49:47 +0300

    Update uyar.js

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -13,8 +13,18 @@ module.exports.run = async (bot, message, args) => {
             let modlog = bot.channels.find(`name`, `lordlog`);
             
             if(!modlog) return message.reply(":warning: **lordlog** `Adında Bir Kanal Bulamıyorum Lütfen Kontrol Et!`");
+            if(reason.lenght < 1) return message.reply(":warning: `Lütfen Bir Sebep Gir`");
             if(message.mentions.users.size < 1) return message.reply(":warning: `Lütfen Uyarmak İstediğin Kullanıcının İsminide Gir`").catch(console.error);
             
+            
+            const uyariembed = new Discord.RichEmbed()
+            
+            .setAuthor("~ U Y A R I ~", message.author.avatarURL)
+            .setColor("#fc9300")
+            .setTimestamp()
+            .setField("Uyarılan", `${user.username}#${user.discriminator}`)
+            .setField("Uyaran", `${message.author.user}#${message.author.discriminator}`);
+            return bot.channels.get(modlog.id).sendEmbed(uyariembed);
             
             
             

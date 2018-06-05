213ed497100ca6627cce993bd8e0477166b42a33
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-05 18:07:26 +0300

    Update clear.js

--- komutlar/clear.js
+++ komutlar/clear.js
@@ -5,6 +5,8 @@ module.exports.run = async (bot, message, args) => {
   //!clear 15
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":warning: `Bunu Yapmaya Yetkin Yok!`");
   if(!args[0]) return message.channel.send(":warning: `Silinecek Mesaj Sayısınıda Yaz`");
+  if(args[0]) > 100 return message.channel.send(":warning: `100'den Fazla Mesaj Silemezsin!`");
+    
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`:white_check_mark: ${args[0]} Adet Mesaj Silindi! `).then(msg => msg.delete(5000));
   });

aab992954ffd20aa1a06e9b73903b4793c98e08e
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-08 17:13:51 +0300

    Update oylamabaşlat.js

--- komutlar/oylamabaşlat.js
+++ komutlar/oylamabaşlat.js
@@ -27,15 +27,15 @@ const embed = new Discord.RichEmbed()
   .addField(":keycap_ten: : 10 Puan.", "Oylamaya Tepki Vererek Katıabilirsin.");
 
   message.channel.send({embed})
-              message.react("1️⃣");
-              message.react("2️⃣");
-              message.react("3️⃣");
-    message.react("4️⃣");
-    message.react("5️⃣");
-    message.react("6️⃣");
-    message.react("7️⃣");
-    message.react("8️⃣");
-    message.react("9️⃣");
+              message.react(":one:");
+              message.react(":two:");
+              message.react(":three:");
+    message.react(":four:");
+    message.react(":five:");
+    message.react(":six:");
+    message.react(":seven:");
+    message.react(":eight:");
+    message.react(":nine:");
     message.react("🔟");
 
   

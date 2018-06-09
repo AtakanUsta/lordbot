37efa9fe847e1eade50786796eeceae515288805
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-09 17:28:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const botconfig = require("./botconfig.json");
-const botconfig = require("./token.json");
+const prefix = require("./prefix.json");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();

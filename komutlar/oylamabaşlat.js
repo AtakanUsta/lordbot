0ab3e13ba5e29a6541112b1c51f77bb702a52912
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-09 18:37:44 +0300

    Update oylamabaşlat.js

--- komutlar/oylamabaşlat.js
+++ komutlar/oylamabaşlat.js
@@ -29,26 +29,7 @@ const embed = new Discord.RichEmbed()
   message.channel.send({embed})
   
   
-  let emoji1 = message.guild.emojis.find('name', "one");
-    let emoji2 = message.guild.emojis.find('name', "two");
-      let emoji3 = message.guild.emojis.find('name', "three");
-        let emoji4 = message.guild.emojis.find('name', "four");
-          let emoji5 = message.guild.emojis.find('name', "five");
-            let emoji6 = message.guild.emojis.find('name', "six");
-              let emoji7 = message.guild.emojis.find('name', "seven");
-                let emoji8 = message.guild.emojis.find('name', "eight");
-                  let emoji9 = message.guild.emojis.find('name', "nine");
   
-              message.react(emoji1);
-              message.react(emoji2);
-              message.react(emoji3);
-    message.react(emoji4);
-    message.react(emoji5);
-    message.react(emoji6);
-    message.react(emoji7);
-    message.react(emoji8);
-    message.react(emoji9);
-    message.react("🔟");
 
   
   

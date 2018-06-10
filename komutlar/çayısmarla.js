ea40f21cd1da8a757eaa6a431f542c7908968ea9
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-10 04:31:38 +0300

    Update çayısmarla.js

--- komutlar/çayısmarla.js
+++ komutlar/çayısmarla.js
@@ -16,7 +16,8 @@ module.exports.run = async (bot, message, args) => {
 
         let sohpet = bot.channels.find(`name`, `💬sohpet`);
         bot.channels.get(sohpet.id).sendEmbed(menu);
-                else
+                
+        if(!message.mentions.users = "@everyone") return;
         let menu = new Discord.RichEmbed()
             .setAuthor("MENÜ")
             .setDescription(`@${message.author.username} Adlı Kişi ${user} Adlı Kişiye Çay Ismarladı`)

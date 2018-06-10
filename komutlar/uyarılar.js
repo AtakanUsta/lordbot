82df20dc9df1d5a155d6c79345c93db82cb81cf5
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-10 22:23:15 +0300

    Add files via upload

---
+++ komutlar/uyarılar.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+
+        //kod başlangıç
+
+        if(!message.member.hasPermissions("MANAGE_MESSAGES")) return message.reply(":warning: `Bunu Yapmaya Yetkin Yok`")
+        let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+        if(!wUser) return message.reply(":warning: `Kimin Uyarılarına Bakmak İstediğinide Gir`")
+        let warnlevel = warns[wUser.id].warns;
+
+        message.reply(`<@${wUser.id}> Adlı Kişinin **${warnlevel}** Uyarısı Bulunmaktadır!`);
+
+
+
+        //kod bitiş
+}
+
+module.exports.help = {
+    name: "uyarılar"
+}

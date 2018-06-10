958fc563b8aeec3357f14edeba821fc371c9314d
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-10 04:42:55 +0300

    Update çayısmarla.js

--- komutlar/çayısmarla.js
+++ komutlar/çayısmarla.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
          if(message.mentions.users.size < 1) return message.reply(":warning: `Lütfen Çay Ismarlamak İstediğin Kullanıcının İsminide Gir`").catch(console.error);
         
      
-        ifnot(message.mentions.users = "@everyone"); return;
+
         let menu2 = new Discord.RichEmbed()
             .setAuthor("MENÜ")
             .setDescription(`@${message.author.username} Adlı Kişi ${user} Adlı Kişiye Çay Ismarladı`)

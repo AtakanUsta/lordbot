1b4a0ee8eabe8c04ddda403218b8b547f51b39a0
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-06-15 09:21:09 +0300

    sohpetiaç ve kapat v1.0 Update Edildi!

---
+++ komutlar/sohpetikapat.js
@@ -0,0 +1,68 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+        //kod başlangıç
+
+        if(!message.member.hasPermissions("ADMINASTOR")) return message.channel.send(":warning: `Bunu Yapmaya Yetkin Yok!`");
+
+        let sohpet = message.guild.channels.find('name', "💬sohbet");
+        let rol = message.guild.roles.find('name', "o");
+
+        let rol2 = message.guild.roles.find('name', "❌Takipçiler❌");
+        let rol3 = message.guild.roles.find('name', "💚Konuşkan Üye💚");
+        let rol4 = message.guild.roles.find('name', "🔥LORD - Takipçi🔥");
+        let rol5 = message.guild.roles.find('name', "💛ATN - Takipçi💛");
+        let rol6 = message.guild.roles.find('name', "🧡Ultra Takipçi🧡");
+        let rol7 = message.guild.roles.find('name', "💜Nihai Takipçi💜");
+        let rol8 = message.guild.roles.find('name', "💚Aktif Üye💚");
+
+            if(!sohpet) return message.channel.send(":warning: Sohpet Kanalını Bulamıyorum!")
+
+            sohpet.overwritePermissions(rol2, {'SEND_MESSAGES': false,})
+             
+            sohpet.overwritePermissions(rol3, {'SEND_MESSAGES': false,})
+           
+            sohpet.overwritePermissions(rol4, {'SEND_MESSAGES': false,})
+        
+            sohpet.overwritePermissions(rol5, {'SEND_MESSAGES': false,})
+             
+            sohpet.overwritePermissions(rol6, {'SEND_MESSAGES': false,})
+           
+            sohpet.overwritePermissions(rol7, {'SEND_MESSAGES': false,})
+        
+            sohpet.overwritePermissions(rol8, {
+                SEND_MESSAGES: false
+            })
+
+
+
+
+            sohpet.overwritePermissions(rol, {
+                SEND_MESSAGES: false
+  
+            });
+
+
+
+
+
+            message.channel.send("#########################################");
+            message.channel.send("#  +----------------------------------------------------------------+  #");
+            message.channel.send("#   |                                  |  Sohpet Susturuldu.  |                           |    #");
+            message.channel.send("#  +----------------------------------------------------------------+  #");
+            message.channel.send("#########################################");
+
+            
+
+            
+
+
+
+        //kod bitiş
+}
+
+module.exports.help = {
+    name: "sohpetikapat"
+}
